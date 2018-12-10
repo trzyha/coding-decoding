21dea04b61882bd93e4bc222daaf9a746a97cdb8
trzyha <39124933+trzyha@users.noreply.github.com>
2018-12-10 11:08:07 +0100

    Add files via upload

---
+++ coded-decoded.py
@@ -0,0 +1,17 @@
+sentence = input ("Input your text: ")
+codedSentence = []
+uncodedSentence = []
+for x in range (0,len(sentence)):
+  #print (sentence[x])
+  senToInt = ord(sentence[x])+3
+  #print (senToInt)
+  senIntToChar = chr(senToInt)
+  #print (senIntToChar)
+  codedSentence.append(senIntToChar) 
+print ("This is coded text: " + "".join(codedSentence))
+
+for x in range (0, len(codedSentence)):
+  senToInt = ord(codedSentence[x])-3
+  senIntToChar = chr(senToInt)
+  uncodedSentence.append(senIntToChar)
+print ("".join(uncodedSentence))
